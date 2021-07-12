b26e38f50bd561a4c271479576fa7bdf94bfff9a
Tu-Trung-Richie Lam <trung_lam@live.com.au>
2021-07-13 01:24:50 +1000

    Completed tutorial #5

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>{{ title }}</h1>
+  <p>Welcome ...</p>
+  <teleport to="#modals" v-if="showModal">
+    <Modal theme="dark" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+        <a href="#">More info</a>
+      </template>
+      <h1>Ninja Giveaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </teleport>
+  <teleport to="#modals" v-if="showModalTwo">
+    <Modal theme="dark" @close="toggleModalTwo">
+      <h1>Sign up to the newsletter</h1>
+      <p>For updates and promo codes!</p>
+    </Modal>
+  </teleport>
+  <button @click="toggleModal">Open Modal</button>
+  <button @click="toggleModalTwo">Open Modal</button>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      title: 'My first Vue app :)',
+      header: 'Sign up for the Giveaway!',
+      text: 'Grab your ninja swag for half price!',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    }
+  }
+}
+</script>
+
+<style>
+#app, #modals {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
